614293af7f897fbcc96238bfad6c7f1a1fa0f857
Dhruv Chawla <dhruv263.dc@gmail.com>
2020-09-28 14:13:40 +0530

    Finished expression AST

--- src/AST.hpp
+++ src/AST.hpp
@@ -4,6 +4,7 @@
 #  define AST_HPP
 
 #include <memory>
+#include <vector>
 #include "Token.hpp"
 
 template <typename T>
@@ -15,5 +16,262 @@ template <typename T>
 using expr_node_t = std::unique_ptr<Expr<T>>;
 template <typename T>
 using stmt_node_t = std::unique_ptr<Stmt<T>>;
+
+// Expression nodes
+
+template <typename T>
+struct AssignExpr;
+template <typename T>
+struct BinaryExpr;
+template <typename T>
+struct CallExpr;
+template <typename T>
+struct GetExpr;
+template <typename T>
+struct GroupingExpr;
+template <typename T>
+struct LiteralExpr;
+template <typename T>
+struct LogicalExpr;
+template <typename T>
+struct SetExpr;
+template <typename T>
+struct SuperExpr;
+template <typename T>
+struct ThisExpr;
+template <typename T>
+struct UnaryExpr;
+template <typename T>
+struct VariableExpr;
+
+// Statement nodes
+
+template <typename T>
+struct BlockStmt;
+template <typename T>
+struct BreakStmt;
+template <typename T>
+struct ClassStmt;
+template <typename T>
+struct ContinueStmt;
+template <typename T>
+struct ExpressionStmt;
+template <typename T>
+struct ForStmt;
+template <typename T>
+struct FuncStmt;
+template <typename T>
+struct IfStmt;
+template <typename T>
+struct ImportStmt;
+template <typename T>
+struct ReturnStmt;
+template <typename T>
+struct SwitchStmt;
+template <typename T>
+struct TypeStmt;
+template <typename T>
+struct VarStmt;
+template <typename T>
+struct WhileStmt;
+
+template <typename T>
+struct Visitor {
+	T visit(AssignExpr<T>& expr) = 0;
+	T visit(BinaryExpr<T>& expr) = 0;
+	T visit(CallExpr<T>& expr) = 0;
+	T visit(GetExpr<T>& expr) = 0;
+	T visit(GroupingExpr<T>& expr) = 0;
+	T visit(LiteralExpr<T>& expr) = 0;
+	T visit(LogicalExpr<T>& expr) = 0;
+	T visit(SetExpr<T>& expr) = 0;
+	T visit(SuperExpr<T>& expr) = 0;
+	T visit(ThisExpr<T>& expr) = 0;
+	T visit(UnaryExpr<T>& expr) = 0;
+	T visit(VariableExpr<T>& expr) = 0;
+	T visit(BlockStmt<T>& stmt) = 0;
+	T visit(BreakStmt<T>& stmt) = 0;
+	T visit(ClassStmt<T>& stmt) = 0;
+	T visit(ContinueStmt<T>& stmt) = 0;
+	T visit(ExpressionStmt<T>& stmt) = 0;
+	T visit(ForStmt<T>& stmt) = 0;
+	T visit(FuncStmt<T>& stmt) = 0;
+	T visit(IfStmt<T>& stmt) = 0;
+	T visit(ImportStmt<T>& stmt) = 0;
+	T visit(ReturnStmt<T>& stmt) = 0;
+	T visit(SwitchStmt<T>& stmt) = 0;
+	T visit(TypeStmt<T>& stmt) = 0;
+	T visit(VarStmt<T>& stmt) = 0;
+	T visit(WhileStmt<T>& stmt) = 0;
+};
+
+template <typename T>
+struct Expr {
+	virtual T accept(Visitor<T>& visitor) = 0;
+	virtual ~Expr() = default;
+};
+
+template <typename T>
+struct Stmt {
+	virtual T accept(Visitor<T>& visitor) = 0;
+	virtual ~Stmt() = default;
+};
+
+template <typename T>
+struct AssignExpr final: public Expr<T> {
+	Token target;
+	expr_node_t<T> value;
+
+	AssignExpr(Token target, expr_node_t<T> value):
+		target{target}, value{std::move(value)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct BinaryExpr final: public Expr<T> {
+	expr_node_t<T> left;
+	Token oper;
+	expr_node_t<T> right;
+
+	BinaryExpr(expr_node_t<T> left, Token oper, expr_node_t<T> right):
+		left{std::move(left)}, oper{oper}, right{std::move(right)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct CallExpr final: public Expr<T> {
+	expr_node_t<T> function;
+	Token paren;
+	std::vector<expr_node_t<T>> args;
+
+	CallExpr(expr_node_t<T> function, Token paren, std::vector<expr_node_t<T>> args):
+		function{std::move(function)}, paren{paren}, args{std::move(args)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct GetExpr final: public Expr<T> {
+	expr_node_t<T> object;
+	Token name;
+
+	GetExpr(expr_node_t<T> object, Token name):
+		object{std::move(object)}, name{name} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct GroupingExpr final: public Expr<T> {
+	expr_node_t<T> expr;
+
+	GroupingExpr(expr_node_t<T> expr):
+		expr{std::move(expr)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct LiteralExpr final: public Expr<T> {
+	T value;
+
+	LiteralExpr(T value):
+		value{std::move(value)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct LogicalExpr final: public Expr<T> {
+	expr_node_t<T> left;
+	Token oper;
+	expr_node_t<T> right;
+
+	LogicalExpr(expr_node_t<T> left, Token oper, expr_node_t<T> right):
+		left{std::move(left)}, oper{oper}, right{std::move(right)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct SetExpr final: public Expr<T> {
+	expr_node_t<T> object;
+	Token name;
+	expr_node_t<T> value;
+
+	SetExpr(expr_node_t<T> object, Token name, expr_node_t<T> value):
+		object{std::move(object)}, name{name}, value{std::move(value)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct SuperExpr final: public Expr<T> {
+	Token keyword;
+	Token name;
+
+	SuperExpr(Token keyword, Token name):
+		keyword{keyword}, name{name} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct ThisExpr final: public Expr<T> {
+	Token keyword;
+
+	ThisExpr(Token keyword):
+		keyword{keyword} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct UnaryExpr final: public Expr<T> {
+	Token operator;
+	expr_node_t<T> right;
+
+	UnaryExpr(Token operator, expr_node_t<T> right):
+		operator{operator}, right{std::move(right)} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
+template <typename T>
+struct VariableExpr final: public Expr<T> {
+	Token name;
+
+	VariableExpr(Token name):
+		name{name} {}
+
+	T accept(Visitor<T>& visitor) override final {
+		return visitor.visit(*this);
+	}
+};
+
 
 #endif
--- src/generate-ast.py
+++ src/generate-ast.py
@@ -1,30 +1,147 @@
 def make_header(file, guard):
     file.write('#pragma once\n\n')
-    file.write('#ifndef ' + guard+ '\n')
+    file.write('#ifndef ' + guard + '\n')
     file.write('#  define ' + guard + '\n\n')
+
 
 def end_file(file):
     file.write('#endif\n')
+
 
 def forward_declare(file, names):
     for name in names:
         file.write('template <typename T>\n')
         file.write('struct ' + name + ';\n')
 
+
 def declare_alias(file, alias, name):
     file.write('template <typename T>\n')
-    file.write('using ' + alias + ' = std::unique_ptr<' + name +'<T>>;\n')
+    file.write('using ' + alias + ' = std::unique_ptr<' + name + '<T>>;\n')
+
+
+def tab(file, n):
+    file.write('\t' * n)
+    return file
+
+
+def declare_visitor(file, visitor, exprs, stmts, expr_base, stmt_base):
+    file.write('template <typename T>\n')
+    file.write('struct ' + visitor + ' {\n')
+    for expr in exprs:
+        tab(file, 1).write(
+            'T visit(' + expr + '<T>& ' + expr_base.lower() + ') = 0;\n')
+
+    for stmt in stmts:
+        tab(file, 1).write(
+            'T visit(' + stmt + '<T>& ' + stmt_base.lower() + ') = 0;\n')
+    file.write('};\n\n')
+
+
+def declare_base(file, base_name):
+    file.write('template <typename T>\n')
+    file.write('struct ' + base_name + ' {\n')
+    tab(file, 1).write('virtual T accept(Visitor<T>& visitor) = 0;\n')
+    tab(file, 1).write('virtual ~' + base_name + '() = default;\n')
+    file.write('};\n\n')
+
+def declare_derived(file, base_name, derived_name, ctor_args, members):
+    file.write('template <typename T>\n')
+    file.write('struct ' + derived_name + ' final: public ' + base_name + '<T> {\n')
+
+    for member in members.split(','):
+        tab(file, 1).write(member.strip() + ';\n')
+    file.write('\n')
+    # Class members
+
+    tab(file, 1).write(derived_name + '(' + members + '):\n')
+    tab(file, 2).write(ctor_args + ' {}\n')
+    file.write('\n')
+    # Class constructor
+
+    tab(file, 1).write('T accept(Visitor<T>& visitor) override final {\n')
+    tab(file, 2).write('return visitor.visit(*this);\n')
+    tab(file, 1).write('}\n')
+    # Accept method
+    file.write('};\n\n')
 
 if __name__ == '__main__':
     with open('AST.hpp', 'wt') as file:
         make_header(file, 'AST_HPP')
         file.write('#include <memory>\n')
+        file.write('#include <vector>\n')
         file.write('#include "Token.hpp"\n\n')
         forward_declare(file, ['Expr', 'Stmt'])
         file.write('\n')
         declare_alias(file, 'expr_node_t', 'Expr')
         declare_alias(file, 'stmt_node_t', 'Stmt')
+        # Base class and alias declarations complete
+
+        Exprs = ['Assign', 'Binary', 'Call', 'Get', 'Grouping', 'Literal',
+                 'Logical', 'Set', 'Super', 'This', 'Unary', 'Variable']
+        Stmts = ['Block', 'Break', 'Class', 'Continue', 'Expression', 'For',
+                 'Func', 'If', 'Import', 'Return', 'Switch', 'Type', 'Var',  'While']
+        Exprs = [x + 'Expr' for x in Exprs]
+        Stmts = [x + 'Stmt' for x in Stmts]
+        file.write('\n// Expression nodes\n\n')
+        forward_declare(file, Exprs)
+        file.write('\n// Statement nodes\n\n')
+        forward_declare(file, Stmts)
         file.write('\n')
-        Exprs = ['Assign', 'Binary', 'Call', 'Get', 'Grouping', 'Literal', 'Logical', 'Set', 'Super', 'This', 'Unary', 'Variable']
-        Stmt = ['Block', 'Class', 'Expression', 'Function', 'If', 'Print', 'Return', 'Val','Var', 'While']
+        # Forward declarations complete
+
+        declare_visitor(file, 'Visitor', Exprs, Stmts, 'Expr', 'Stmt')
+        # Visitor declaration complete
+
+        declare_base(file, 'Expr')
+        declare_base(file, 'Stmt')
+
+        declare_derived(file, 'Expr', Exprs[0],
+            'target{target}, value{std::move(value)}',
+            'Token target, expr_node_t<T> value')
+
+        declare_derived(file, 'Expr', Exprs[1],
+            'left{std::move(left)}, oper{oper}, right{std::move(right)}',
+            'expr_node_t<T> left, Token oper, expr_node_t<T> right')
+
+        declare_derived(file, 'Expr', Exprs[2],
+            'function{std::move(function)}, paren{paren}, args{std::move(args)}',
+            'expr_node_t<T> function, Token paren, std::vector<expr_node_t<T>> args')
+
+        declare_derived(file, 'Expr', Exprs[3],
+            'object{std::move(object)}, name{name}',
+            'expr_node_t<T> object, Token name')
+        
+        declare_derived(file, 'Expr', Exprs[4],
+            'expr{std::move(expr)}',
+            'expr_node_t<T> expr')
+
+        declare_derived(file, 'Expr', Exprs[5],
+            'value{std::move(value)}',
+            'T value')
+
+        declare_derived(file, 'Expr', Exprs[6],
+            'left{std::move(left)}, oper{oper}, right{std::move(right)}',
+            'expr_node_t<T> left, Token oper, expr_node_t<T> right')
+
+        declare_derived(file, 'Expr', Exprs[7],
+            'object{std::move(object)}, name{name}, value{std::move(value)}',
+            'expr_node_t<T> object, Token name, expr_node_t<T> value')
+
+        declare_derived(file, 'Expr', Exprs[8],
+            'keyword{keyword}, name{name}',
+            'Token keyword, Token name')
+
+        declare_derived(file, 'Expr', Exprs[9],
+            'keyword{keyword}',
+            'Token keyword')
+
+        declare_derived(file, 'Expr', Exprs[10],
+            'operator{operator}, right{std::move(right)}',
+            'Token operator, expr_node_t<T> right')
+
+        declare_derived(file, 'Expr', Exprs[11],
+            'name{name}',
+            'Token name')
+
+        file.write('\n')
         end_file(file)
