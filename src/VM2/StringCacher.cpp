9bd18b09609b939079e55b52651cd52e1f5cf3d0
dc03 <dhruv263.dc@gmail.com>
2021-07-07 11:34:56 +0530

    Redo string interning implementation

--- src/VM2/StringCacher.cpp
+++ src/VM2/StringCacher.cpp
@@ -5,20 +5,40 @@
 #include <algorithm>
 
 const HashedString &StringCacher::concat(const HashedString &first, const HashedString &second) {
-    HashPair pair = {first.hash, second.hash};
-    if (auto it = table.find(pair); it != table.end()) {
-        return *(it->second);
+    return insert(first.str + second.str);
+}
+
+const HashedString &StringCacher::insert(std::string &&value) {
+    return insert(HashedString{std::move(value)});
+}
+
+const HashedString &StringCacher::insert(const std::string &value) {
+    return insert(HashedString{value});
+}
+
+const HashedString &StringCacher::insert(HashedString &&value) {
+    if (auto it = strings.find(value); it != strings.end()) {
+        it->second++;
+        return it->first;
     } else {
-        std::string value = first.str + second.str;
-        if (auto it2 = std::find(strings.begin(), strings.end(), value); it2 != strings.end()) {
-            table.emplace(pair, &(*it2));
-            return *it2;
-        } else {
-            return *strings.emplace(std::move(value)).first;
-        }
+        return strings.emplace(std::move(value), 1).first->first;
     }
 }
 
-const HashedString &StringCacher::insert(std::string value) {
-    return *strings.emplace(std::move(value)).first;
+const HashedString &StringCacher::insert(const HashedString &value) {
+    if (auto it = strings.find(value); it != strings.end()) {
+        it->second++;
+        return it->first;
+    } else {
+        return strings.emplace(value, 1).first->first;
+    }
+}
+
+void StringCacher::remove(const HashedString &value) {
+    if (auto it = strings.find(value); it != strings.end()) {
+        it->second--;
+        if (it->second <= 0) {
+            strings.erase(it);
+        }
+    }
 }
--- src/VM2/StringCacher.hpp
+++ src/VM2/StringCacher.hpp
@@ -6,8 +6,6 @@
 #ifndef VM2_STRING_CACHER_HPP
 #define VM2_STRING_CACHER_HPP
 
-#include "HashTable.hpp"
-
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -20,32 +18,15 @@ struct HashedString {
     HashedString() noexcept = default;
     explicit HashedString(std::string str) : str{std::move(str)}, hash{std::hash<std::string>{}(this->str)} {}
 
-    [[nodiscard]] bool operator==(const HashedString &other) const noexcept { return str == other.str; }
+    [[nodiscard]] bool operator==(const HashedString &other) const noexcept {
+        return hash == other.hash && str == other.str;
+    }
     [[nodiscard]] bool operator>(const HashedString &other) const noexcept { return str > other.str; }
     [[nodiscard]] bool operator<(const HashedString &other) const noexcept { return str < other.str; }
     [[nodiscard]] bool operator==(const std::string &other) const noexcept { return str == other; }
 };
 
-using HashPair = std::pair<std::size_t, std::size_t>;
-
-// From the Boost libraries
-template <typename T>
-constexpr static inline void hash_combine(std::size_t &seed, const T &v) {
-    std::hash<T> hasher;
-    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-
 namespace std {
-template <>
-struct hash<HashPair> {
-    std::size_t operator()(const HashPair &pair) const noexcept {
-        std::size_t seed = 0;
-        hash_combine(seed, pair.first);
-        hash_combine(seed, pair.second);
-        return seed;
-    }
-};
-
 template <>
 struct hash<HashedString> {
     std::size_t operator()(const HashedString &string) const noexcept { return string.hash; }
@@ -53,14 +34,17 @@ struct hash<HashedString> {
 } // namespace std
 
 class StringCacher {
-    std::unordered_set<HashedString> strings;
-    std::unordered_map<HashPair, const HashedString *> table{};
+    std::unordered_map<HashedString, std::size_t> strings{};
 
   public:
     StringCacher() noexcept = default;
 
     [[nodiscard]] const HashedString &concat(const HashedString &first, const HashedString &second);
-    [[nodiscard]] const HashedString &insert(std::string value);
+    [[nodiscard]] const HashedString &insert(std::string &&value);
+    [[nodiscard]] const HashedString &insert(const std::string &value);
+    [[nodiscard]] const HashedString &insert(HashedString &&value);
+    [[nodiscard]] const HashedString &insert(const HashedString &value);
+    void remove(const HashedString &value);
 };
 
 #endif
