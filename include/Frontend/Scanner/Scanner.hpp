7199de35e2a7e0be8892776d57ea43ef63d162c7
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-10-06 10:43:24 +0530

    [refactor] (ScannerV2) : change implementation a little

--- include/Frontend/Scanner/Scanner.hpp
+++ include/Frontend/Scanner/Scanner.hpp
@@ -51,30 +51,81 @@ class ScannerV2 {
     std::size_t current_token_end{};
 
     std::size_t paren_depth{};
-    std::vector<Token> tokens{};
+    Token previous{};
+    Token current{};
     std::string_view source{};
 
-    std::unordered_map<std::string, TokenType> keyword_map{};
-    Trie keywords{};
+    struct KeywordTypePair {
+        const char *lexeme;
+        TokenType type;
+    };
+
+    constexpr static std::array keywords{
+        // clang-format off
+        KeywordTypePair{"and", TokenType::AND},
+        KeywordTypePair{"bool", TokenType::BOOL},
+        KeywordTypePair{"break", TokenType::BREAK},
+        KeywordTypePair{"class", TokenType::CLASS},
+        KeywordTypePair{"const", TokenType::CONST},
+        KeywordTypePair{"continue", TokenType::CONTINUE},
+        KeywordTypePair{"default", TokenType::DEFAULT},
+        KeywordTypePair{"else", TokenType::ELSE},
+        KeywordTypePair{"false", TokenType::FALSE},
+        KeywordTypePair{"float", TokenType::FLOAT},
+        KeywordTypePair{"fn", TokenType::FN},
+        KeywordTypePair{"for", TokenType::FOR},
+        KeywordTypePair{"if", TokenType::IF},
+        KeywordTypePair{"import", TokenType::IMPORT},
+        KeywordTypePair{"int", TokenType::INT},
+        KeywordTypePair{"move", TokenType::MOVE},
+        KeywordTypePair{"null", TokenType::NULL_},
+        KeywordTypePair{"not", TokenType::NOT},
+        KeywordTypePair{"or", TokenType::OR},
+        KeywordTypePair{"protected", TokenType::PROTECTED},
+        KeywordTypePair{"private", TokenType::PRIVATE},
+        KeywordTypePair{"public", TokenType::PUBLIC},
+        KeywordTypePair{"ref", TokenType::REF},
+        KeywordTypePair{"return", TokenType::RETURN},
+        KeywordTypePair{"string", TokenType::STRING},
+        KeywordTypePair{"super", TokenType::SUPER},
+        KeywordTypePair{"switch", TokenType::SWITCH},
+        KeywordTypePair{"this", TokenType::THIS},
+        KeywordTypePair{"true", TokenType::TRUE},
+        KeywordTypePair{"type", TokenType::TYPE},
+        KeywordTypePair{"typeof", TokenType::TYPEOF},
+        KeywordTypePair{"var", TokenType::VAR},
+        KeywordTypePair{"while", TokenType::WHILE}
+        // clang-format on
+    };
+
+    Trie keyword_map{};
 
     [[nodiscard]] bool is_at_end() const noexcept;
 
-    [[nodiscard]] char advance();
+    char advance();
     [[nodiscard]] char peek() const noexcept;
     [[nodiscard]] char peek_next() const noexcept;
-    [[nodiscard]] const Token &previous() const noexcept;
     bool match(char ch);
 
-    void scan_number();
-    void scan_identifier_or_keyword();
-    void scan_string();
+    [[nodiscard]] Token make_token(TokenType type, std::string_view lexeme) const;
+
+    Token scan_number();
+    Token scan_identifier_or_keyword();
+    Token scan_string();
+
+    void skip_comment();
     void skip_multiline_comment();
+
+    bool is_valid_eol(const Token &token);
+
+    std::string_view current_token_lexeme();
 
   public:
     ScannerV2();
+    explicit ScannerV2(std::string_view source);
 
     Token scan_token();
-    std::vector<Token> &scan_all();
+    std::vector<Token> scan_all();
 };
 
 #endif
