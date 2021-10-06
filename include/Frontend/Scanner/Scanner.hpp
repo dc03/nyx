95b603b386e43fa1927572e4fb99344c988b33f7
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-10-06 19:29:20 +0530

    [refactor] (Scanner) : remove old Scanner

--- include/Frontend/Scanner/Scanner.hpp
+++ include/Frontend/Scanner/Scanner.hpp
@@ -14,37 +14,6 @@
 #include <unordered_map>
 #include <vector>
 
-class Scanner {
-    std::size_t line{1};
-    std::size_t start{};
-    std::size_t current{};
-    std::size_t paren_count{};
-    std::vector<Token> tokens{};
-    std::string_view source{};
-    Trie keywords{};
-
-  public:
-    Scanner();
-    explicit Scanner(std::string_view source);
-
-    [[nodiscard]] bool is_at_end() const noexcept;
-
-    char advance();
-    [[nodiscard]] char peek() const noexcept;
-    [[nodiscard]] char peek_next() const noexcept;
-    [[nodiscard]] const Token &previous() const noexcept;
-    bool match(char ch);
-
-    void number();
-    void identifier();
-    void string(char delimiter);
-    void multiline_comment();
-
-    void add_token(TokenType type);
-    void scan_token();
-    const std::vector<Token> &scan();
-};
-
 class ScannerV2 {
     std::size_t line{1};
     std::size_t current_token_start{};
