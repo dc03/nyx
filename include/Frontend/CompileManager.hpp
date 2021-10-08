4c9fd44ff0c16575a87f8e89514dd7ea7cf4ebc3
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-10-08 12:22:12 +0530

    [feature] (CompileManager) : Implement CompileManager

--- include/Frontend/CompileManager.hpp
+++ include/Frontend/CompileManager.hpp
@@ -12,6 +12,8 @@
 #include "Frontend/Parser/TypeResolver.hpp"
 #include "Frontend/Scanner/Scanner.hpp"
 
+#include <filesystem>
+
 class CompileManager {
     CompileContext *ctx{};
 
@@ -23,7 +25,16 @@ class CompileManager {
 
   public:
     CompileManager() noexcept = default;
-    CompileManager(std::string_view module_path);
+    CompileManager(CompileContext *ctx, std::filesystem::path path, bool is_main, std::size_t module_depth = 0);
+
+    void parse_module();
+    void check_module();
+
+    std::string &module_name();
+    std::filesystem::path module_path() const;
+
+    Module &get_module();
+    Module move_module();
 };
 
 #endif
---
+++ src/Frontend/CompileManager.cpp
@@ -0,0 +1,76 @@
+/* Copyright (C) 2021  Dhruv Chawla */
+/* See LICENSE at project root for license details */
+#include "Frontend/CompileManager.hpp"
+
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
+CompileManager::CompileManager(CompileContext *ctx, fs::path path, bool is_main, std::size_t module_depth) : ctx{ctx} {
+    if (is_main) {
+        ctx->main_parent_path = fs::absolute(path).parent_path();
+    } else {
+        path = ctx->main_parent_path.append(path.c_str());
+    }
+
+    if (fs::is_directory(path)) {
+        compile_error({"'", std::string{path}, "' represents a directory, not a file"});
+        return;
+    } else if (not fs::exists(path)) {
+        compile_error({"No such file: '", std::string{path}, "'"});
+        return;
+    }
+
+    fs::path module_path = fs::absolute(path);
+
+    fs::path module_name = module_path.filename();
+    fs::path module_parent = module_path.parent_path();
+
+    if (is_main) {
+        ctx->main_parent_path = module_parent;
+    }
+
+    std::ifstream file(module_path, std::ios::in);
+    if (not file.is_open()) {
+        compile_error({"Unable to open module '", module_name.c_str(), "'"});
+    }
+
+    std::string source{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
+
+    logger.set_module_name(module_name.native());
+    logger.set_source(source);
+
+    module = Module{module_name.c_str(), module_parent, source};
+    if (is_main) {
+        ctx->main = &module;
+    }
+    scanner.set_source(module.source);
+
+    parser = Parser{ctx, &scanner, &module, module_depth};
+    resolver = TypeResolver{ctx, &module};
+}
+
+void CompileManager::parse_module() {
+    module.statements = parser.program();
+}
+
+void CompileManager::check_module() {
+    resolver.check(module.statements);
+}
+
+std::string &CompileManager::module_name() {
+    return module.name;
+}
+
+fs::path CompileManager::module_path() const {
+    return module.module_directory;
+}
+
+Module &CompileManager::get_module() {
+    return module;
+}
+
+Module CompileManager::move_module() {
+    return std::move(module);
+}
