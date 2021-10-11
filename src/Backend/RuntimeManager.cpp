04ebfc1594c1777ea2fcce6ba2b847c4965d4bf7
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-10-11 12:48:56 +0530

    [refactor] (RuntimeManager) : update

--- src/Backend/RuntimeManager.cpp
+++ src/Backend/RuntimeManager.cpp
@@ -12,19 +12,22 @@ RuntimeManager::RuntimeManager(RuntimeContext *ctx) : ctx{ctx} {
 }
 
 void RuntimeManager::compile(CompileContext *compile_ctx) {
-    std::sort(compile_ctx->parsed_modules.begin(), compile_ctx->parsed_modules.end(),
-        [](const auto &x1, const auto &x2) { return x1.second > x2.second; });
+    compile_ctx->sort_modules();
+
+    for (std::size_t i = 0; i < compile_ctx->parsed_modules.size(); i++) {
+        ctx->module_path_map[compile_ctx->parsed_modules[i].first.full_path.c_str()] = i;
+    }
 
     generator.set_compile_ctx(compile_ctx);
+
+    for (auto &[module, depth] : compile_ctx->parsed_modules) {
+        ctx->compiled_modules.emplace_back(generator.compile(module));
+    }
 
     if (compile_ctx->main != nullptr) {
         main = generator.compile(*compile_ctx->main);
         main.top_level_code.emit_instruction(Instruction::HALT, 0);
         ctx->main = &main;
-    }
-
-    for (auto &[module, depth] : compile_ctx->parsed_modules) {
-        ctx->compiled_modules.emplace_back(generator.compile(module));
     }
 }
 
@@ -33,6 +36,14 @@ void RuntimeManager::disassemble() {
 }
 
 void RuntimeManager::run() {
+    if (ctx->main != nullptr) {
+        vm.set_function_module_pointers(ctx->main);
+    }
+
+    for (auto &module : ctx->compiled_modules) {
+        vm.set_function_module_pointers(&module);
+    }
+
     if (ctx->main != nullptr) {
         vm.run(main);
     }
