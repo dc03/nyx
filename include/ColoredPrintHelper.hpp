87068fda6aaf442350eada7b8557f8fd194831b4
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-11-08 08:50:48 +0530

    [feature] (ColoredPrintHelper) : add new helper class

---
+++ include/ColoredPrintHelper.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+/* Copyright (C) 2021  Dhruv Chawla */
+/* See LICENSE at project root for license details */
+
+#ifndef COLORED_PRINT_HELPER_HPP
+#define COLORED_PRINT_HELPER_HPP
+
+#include <iostream>
+
+struct ColoredPrintHelper {
+    using StreamColorModifier = std::ostream &(*)(std::ostream &out);
+    bool colors_enabled{};
+    StreamColorModifier colorizer{};
+
+    friend std::ostream &operator<<(std::ostream &out, const ColoredPrintHelper &helper) {
+        if (helper.colors_enabled) {
+            return out << helper.colorizer;
+        } else {
+            return out;
+        }
+    }
+};
+
+#endif
