ce4f89d24581c5eee598a2140106abc3431da811
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2022-01-02 19:18:11 +0530

    [refactor] (Token) : add `const` to equality operators

--- include/AST/Token.hpp
+++ include/AST/Token.hpp
@@ -22,8 +22,8 @@ struct Token {
     Token(TokenType type, std::string lexeme, std::size_t line, std::size_t start, std::size_t end)
         : type{type}, lexeme{std::move(lexeme)}, line{line}, start{start}, end{end} {}
 
-    bool operator==(const Token &other) { return lexeme == other.lexeme; }
-    bool operator!=(const Token &other) { return lexeme != other.lexeme; }
+    bool operator==(const Token &other) const { return lexeme == other.lexeme; }
+    bool operator!=(const Token &other) const { return lexeme != other.lexeme; }
 };
 
 #endif
